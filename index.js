63acdff0d6c10ab865f945e77b431b790535692b
bigtoe <xxxlata@naver.com>
2021-12-03 17:22:41 +0900

    clone

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const menu = document.querySelector(".hamburger_menu span");
 const menuShow = document.querySelector(".hamburger_menu_show");
 
-function show(){
+function show(event){
+    event.preventDefault();
     menuShow.style.display = "block";
 }
 
 menu.addEventListener("click",show);
+menu.addEventListener("submit",show);
+
 
